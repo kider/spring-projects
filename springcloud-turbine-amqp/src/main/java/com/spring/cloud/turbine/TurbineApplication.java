f320bc2e65dd1234adc37aae0c7005915a39d452
kider <531822296@qq.com>
2018-03-16 15:21:27 +0800

    1、修改turbine,用http方式聚合时引用jar包路径错误问题。 2、添加并发httpClient代码，便于测试。

--- springcloud-turbine-amqp/src/main/java/com/spring/cloud/turbine/TurbineApplication.java
+++ springcloud-turbine-amqp/src/main/java/com/spring/cloud/turbine/TurbineApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
-//@EnableTurbine //http方式
-@EnableTurbineStream //mq 方式
+@EnableTurbine //http方式
+//@EnableTurbineStream //mq 方式
 @EnableDiscoveryClient
 @SpringBootApplication
 public class TurbineApplication {
