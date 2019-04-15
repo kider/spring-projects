0c2c007ea166eb274e9b14d3e83c4fe7b352c574
chenhao <chenhao-ds6@gome.com.cn>
2019-04-15 18:14:40 +0800

    修改配置，处理乱码问题。

--- springboot-all/src/main/java/com/spring/boot/example/SpringbootDemoApplication.java
+++ springboot-all/src/main/java/com/spring/boot/example/SpringbootDemoApplication.java
@@ -1,18 +1,8 @@
 package com.spring.boot.example;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.Http11Nio2Protocol;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -20,19 +10,17 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableScheduling
 @EnableAspectJAutoProxy
-@EnableAutoConfiguration
 @EnableTransactionManagement
 @EnableCaching
 public class SpringbootDemoApplication {
 
-	public static void main(String[] args) {
-//		SpringApplication.run(SpringbootDemoApplication.class, args);
-		SpringApplication springApplication = new SpringApplication(SpringbootDemoApplication.class);
-		springApplication.run(args);
-	}
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(SpringbootDemoApplication.class);
+        springApplication.run(args);
+    }
 
-	//自定义 嵌入式web容器配置
-	@Bean
+    //自定义 嵌入式web容器配置
+	/*@Bean
 	public static EmbeddedServletContainerCustomizer embeddedServletContainerCustomizer() {
 		return new EmbeddedServletContainerCustomizer() {
 
@@ -62,5 +50,5 @@ public class SpringbootDemoApplication {
 
 			}
 		};
-	};
+	};*/
 }
