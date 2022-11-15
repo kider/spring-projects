1319286fe22448b167076d343fb5da05215a4540
chenhao26 <chenhao26@gome.com.cn>
2022-11-15 19:08:37 +0800

    mybatis批量测试

--- springboot-all/src/main/java/com/spring/boot/example/mapper/UserMapper.java
+++ springboot-all/src/main/java/com/spring/boot/example/mapper/UserMapper.java
@@ -2,18 +2,22 @@ package com.spring.boot.example.mapper;
 
 
 import com.spring.boot.example.dto.UserDto;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
+import com.spring.boot.example.entity.User;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
-@Mapper
+import java.util.List;
+
+@Repository
 public interface UserMapper {
 
-    @Select("SELECT * FROM t_user WHERE NAME = #{name}")
+    //@Select("SELECT * FROM t_user WHERE NAME = #{name}")
     UserDto findByName(@Param("name") String name);
 
-    @Insert("INSERT INTO t_user(NAME, AGE) VALUES(#{name}, #{age})")
-    int insert(@Param("name") String name, @Param("age") Integer age);
+    //@Insert("INSERT INTO t_user(NAME, AGE) VALUES(#{name}, #{age})")
+    int insertOne(@Param("name") String name, @Param("age") Integer age);
+
+
+    int insertOneSQL(List<User> users);
 
 }
--- springboot-all/src/test/java/com/spring/boot/example/MyBatisApplicationTests.java
+++ springboot-all/src/test/java/com/spring/boot/example/MyBatisApplicationTests.java
@@ -4,6 +4,7 @@ import com.spring.boot.example.dao.CityDao;
 import com.spring.boot.example.dao.HotelDao;
 import com.spring.boot.example.dto.UserDto;
 import com.spring.boot.example.entity.Hotel;
+import com.spring.boot.example.entity.User;
 import com.spring.boot.example.mapper.UserMapper;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
@@ -16,15 +17,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringbootDemoApplication.class)
 public class MyBatisApplicationTests {
-
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private CityDao cityDao;
@@ -35,16 +34,67 @@ public class MyBatisApplicationTests {
     @Autowired
     SqlSessionFactory sqlSessionFactory;
 
+    @Autowired
+    private UserMapper userMapper;
+
 
     @Test
-    @Rollback
     public void testBatchInsert() throws Exception {
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 5000; i++) {
+            User user = new User();
+            user.setName("chenhao" + i);
+            user.setAge(i);
+            userMapper.insertOne("chenhao" + i, i);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.printf("共消耗时间:%s ms", endTime - startTime);
+    }
+
+    @Test
+    public void testBatchInsert1() throws Exception {
+        long startTime = System.currentTimeMillis();
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 5000; i++) {
+            User user = new User();
+            user.setName("chenhao" + i);
+            user.setAge(i);
+            users.add(user);
+        }
+        userMapper.insertOneSQL(users);
+        long endTime = System.currentTimeMillis();
+        System.out.printf("共消耗时间:%s ms", endTime - startTime);
+    }
+
+    @Test
+    public void testBatchInsert2() throws Exception {
         long startTime = System.currentTimeMillis();
         SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH);
         UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
         for (int i = 0; i < 5000; i++) {
-            userMapper.insert("chenhao" + i, i);
+            User user = new User();
+            user.setName("chenhao" + i);
+            user.setAge(i);
+            userMapper.insertOne("chenhao" + i, i);
         }
+        sqlSession.commit();
+        long endTime = System.currentTimeMillis();
+        System.out.printf("共消耗时间:%s ms", endTime - startTime);
+    }
+
+    @Test
+    public void testBatchInsert3() throws Exception {
+        long startTime = System.currentTimeMillis();
+        SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH);
+        UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+        List<User> users = new ArrayList<>();
+        for (int i = 0; i < 5000; i++) {
+            User user = new User();
+            user.setName("chenhao" + i);
+            user.setAge(i);
+            users.add(user);
+        }
+        userMapper.insertOneSQL(users);
         sqlSession.commit();
         long endTime = System.currentTimeMillis();
         System.out.printf("共消耗时间:%s ms", endTime - startTime);
@@ -54,7 +104,7 @@ public class MyBatisApplicationTests {
     @Test
     @Rollback
     public void testMyBatis1() throws Exception {
-        userMapper.insert("chenhao", 28);
+        userMapper.insertOne("chenhao", 28);
         UserDto u = userMapper.findByName("chenhao");
         Assert.assertEquals(28, u.getAge().intValue());
     }
